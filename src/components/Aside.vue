c2243a23038769f2989dcaa94eb86bb10d154bfe
mathesbh <mathesbh@gmail.com>
2021-04-13 18:42:58 -0300

    Inclui componente Aside

---
+++ src/components/Aside.vue
@@ -0,0 +1,22 @@
+<template>
+  <aside >
+    <section v-for="(item, index) in items" :key="index">
+      <h5>{{ item }}</h5>
+    </section>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'Aside',
+  props: {
+    items: String,
+  },
+}
+</script>
+
+<style scoped>
+  aside{
+    text-align: center;
+  }
+</style>
