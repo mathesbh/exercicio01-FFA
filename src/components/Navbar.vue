49bc23c75e9de1184a683eba36bc69a5fa831e5c
mathesbh <mathesbh@gmail.com>
2021-04-13 08:32:50 -0300

    Inclui componente Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data(){
+    return {
+      items: ['Section 1', 'Section 2', 'Section 3', 'Section 4']
+    }
+  }
+}
+</script>
+
+<style scoped>
+  ul{
+    display: flex;
+    list-style-type: none;
+    justify-content: space-around;
+    padding: 2% 15%;
+    background-color: #2c3e50;
+    color: aliceblue;
+  }
+</style>
