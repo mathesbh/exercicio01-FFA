941f4689c22c78fa5891f34f5dcf1c124dfe305e
mathesbh <mathesbh@gmail.com>
2021-04-13 18:16:14 -0300

    Inclui componente Picture

---
+++ src/components/Picture.vue
@@ -0,0 +1,22 @@
+<template>
+    <picture>
+      <img v-bind:src="link" v-bind:alt="title">
+    </picture>
+</template>
+
+<script>
+export default {
+  name: 'Picture',
+  props: {
+    link: String,
+    title: String,
+  },  
+}
+</script>
+
+<style scoped>
+picture img{
+  width: 250px;
+  height: 230px;
+}
+</style>
